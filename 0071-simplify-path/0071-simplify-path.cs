4d5278a1c2a68fc26a82c2a6822dfe9e20b2a9e8
Mina Shoukrala <165972928+minashoukrala@users.noreply.github.com>
2024-11-03 17:16:34 -0800

    Time: 2 ms (95.38%), Space: 41.6 MB (50.75%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+public class Solution {
+    public string SimplifyPath(string path) {
+        Stack<string> MyPath=new Stack<string>();
+        string[] words = path.Split('/');
+        StringBuilder result = new StringBuilder();
+
+        foreach(var s in words){
+            
+            if(s == "..") {if(MyPath.Count > 0) MyPath.Pop();}
+            else if(s == "." || s == "") continue;
+            else MyPath.Push(s);
+        }
+
+
+        while(MyPath.Count > 0){
+
+            result.Insert(0, MyPath.Pop());
+            result.Insert(0,"/");
+
+        }
+
+        if(result.Equals(""))return "/";
+
+        return result.ToString();
+        
+    }
+}
