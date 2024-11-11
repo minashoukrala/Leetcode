cda5a5536f044ae6cbe6ed717d3fe7fb43c54282
Mina Shoukrala <165972928+minashoukrala@users.noreply.github.com>
2024-11-10 16:22:12 -0800

    Time: 0 ms (100%), Space: 42.2 MB (9.04%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cs
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -7,17 +7,17 @@ public class Solution {
         if(end < 0 ) return 0;
 
         while(start <= end){
-            mid = (start + end) / 2;
+            mid = start + (end - start) / 2;
 
             if(nums[mid] == target) return mid;
 
             else if(nums[mid] > target){
                 end = mid - 1;
-            }else if(nums[mid] < target){
+            }else {
                 start = mid + 1;
             }
 
         }
-        return mid + 1;
+        return start;
     }
 }
