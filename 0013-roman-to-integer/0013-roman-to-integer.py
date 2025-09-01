4530d79a25893f0d00f7a7d06ffb4286f135b898
Mina Shoukrala <165972928+minashoukrala@users.noreply.github.com>
2025-09-01 13:02:02 -0700

    Time: 14 ms (5.56%), Space: 17.8 MB (39.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,11 +4,10 @@ class Solution:
         romanMapping = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "IV":4, "IX":9,"XL":40,"XC":90,"CD":400, "CM":900}
 
         ans = 0
+        i = 0
 
         if not s:
             return 0
-        
-        i = 0
 
         while i < len(s):
             
@@ -22,9 +21,6 @@ class Solution:
                 
                 i += 1
                
-     
-         
-        
         return ans
             
         
